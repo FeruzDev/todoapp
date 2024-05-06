60accfa6b49a8cb7c5405870a5ce5bcc9cd4d5f7
FeruzDev <60344023+FeruzDev@users.noreply.github.com>
2024-05-06 15:24:44 +0500

    full completed upload

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+            <div className='navbar-main'>
+                <h3>
+                    Mobile Solutions
+                </h3>
+            </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/Notification/notificationExample.js
@@ -0,0 +1,20 @@
+addNotification(
+    {
+        title: "Title",
+        description: "Description",
+        text: "Text",
+        status: "success" || "failed" || "warning",
+        background: "purple" || "#800080",
+        color: "white" || "#FFFFFF",
+        icon: "✅" || <FaCheckCircle />,
+        duration : '5s', // Default is 5s
+        iconColor: "white" || "#FFFFFF",
+        textsColor: "white" || "#FFFFFF",
+        titleColor: "white" || "#FFFFFF",
+        textColor: "white" || "#FFFFFF",
+        descriptionColor: "white" || "#FFFFFF",
+        fixed: true,
+        progressColor: "gray" || "#808080",
+        closeButtonRotate: true,
+    }
+);
